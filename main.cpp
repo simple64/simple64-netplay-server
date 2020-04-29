f00d3ddf845cab8ec64102c7b4c0d368c9ada8ed
Logan McNaughton <logan@bacoosta.com>
2020-04-29 08:27:32 -0600

    put udp server back for testing

--- main.cpp
+++ main.cpp
@@ -1,11 +1,15 @@
 #include <QCoreApplication>
 #include "socketServer.h"
+#include "udpServer.h"
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
-    SocketServer server;
+    SocketServer socketServer;
+
+    UdpServer udpServer;
+    udpServer.initSocket();
 
     return a.exec();
 }
