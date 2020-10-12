1843411dd8d6e19b61332fe2d35ab6e0ecda2bf8
Logan McNaughton <logan@bacoosta.com>
2020-10-11 18:52:42 -0700

    Update motd

--- socketServer.cpp
+++ socketServer.cpp
@@ -212,7 +212,7 @@ void SocketServer::processBinaryMessage(QByteArray message)
     else if (json.value("type").toString() == "get_motd")
     {
         room.insert("type", "send_motd");
-        room.insert("message", QStringLiteral("Have fun!"));
+        room.insert("message", QStringLiteral("Join <a href=\"https://discord.gg/B5svWhR\">The Discord Server</a> for voice chat!"));
         json_doc = QJsonDocument(room);
         client->sendBinaryMessage(json_doc.toJson());
     }
