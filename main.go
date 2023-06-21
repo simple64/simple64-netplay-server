37b6ec8bfe8e76e092b89eaec5915174b0d10c6f
Logan McNaughton <848146+loganmc10@users.noreply.github.com>
2023-06-20 20:48:20 -0600

    add option to write logs to file

--- main.go
+++ main.go
@@ -13,17 +13,28 @@ import (
 
 const DefaultBasePort = 45000
 
+func newZap(logPath string) (*zap.Logger, error) {
+	cfg := zap.NewProductionConfig()
+	cfg.OutputPaths = []string{"stderr"}
+	if logPath != "" {
+		cfg.OutputPaths = append(cfg.OutputPaths, logPath)
+	}
+	return cfg.Build()
+}
+
 func main() {
-	zapLog, err := zap.NewProduction()
+	name := flag.String("name", "local-server", "Server name")
+	basePort := flag.Int("baseport", DefaultBasePort, "Base port")
+	disableBroadcast := flag.Bool("disable-broadcast", false, "Disable LAN broadcast")
+	logPath := flag.String("log-path", "", "Write logs to this file")
+	flag.Parse()
+
+	zapLog, err := newZap(*logPath)
 	if err != nil {
 		log.Panic(err)
 	}
 	logger := zapr.NewLogger(zapLog)
 
-	name := flag.String("name", "local-server", "Server name")
-	basePort := flag.Int("baseport", DefaultBasePort, "Base port")
-	disableBroadcast := flag.Bool("disable-broadcast", false, "Disable LAN broadcast")
-	flag.Parse()
 	if *name == "" {
 		logger.Error(fmt.Errorf("name required"), "server name not set")
 		os.Exit(1)
